5ed444ef8be3ae7257ee43084e94b4ee717f4cae
jask1m <jkim94960@gmail.com>
2024-08-12 16:53:23 -0700

    backend deployment

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+}));
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
